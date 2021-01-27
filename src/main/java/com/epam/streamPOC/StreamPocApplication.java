25f80d36582abf5781d0c7058d085dce79894726
Enrique De Lavalle <enrique_delavalle@epam.com>
2021-01-27 13:10:55 +0100

    edelavalle first commit

---
+++ src/main/java/com/epam/streamPOC/StreamPocApplication.java
@@ -0,0 +1,13 @@
+package com.epam.streamPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamPocApplication.class, args);
+	}
+
+}
