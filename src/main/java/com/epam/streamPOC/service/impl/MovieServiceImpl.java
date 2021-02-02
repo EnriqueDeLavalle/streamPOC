9959c6b2dc80e08291de13f59deeb8ef7c366a1d
Lukasz_Galeziowski <lukasz_galeziowski@epam.com>
2021-02-02 18:17:01 +0100

    Code refactor

--- src/main/java/com/epam/streamPOC/service/impl/MovieServiceImpl.java
+++ src/main/java/com/epam/streamPOC/service/impl/MovieServiceImpl.java
@@ -1,76 +1,77 @@
 package com.epam.streamPOC.service.impl;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.function.BiPredicate;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.epam.streamPOC.dao.MovieDao;
 import com.epam.streamPOC.domain.Director;
 import com.epam.streamPOC.domain.Movie;
 import com.epam.streamPOC.dto.DirectorDTO;
 import com.epam.streamPOC.dto.MovieDTO;
 import com.epam.streamPOC.service.MovieService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieServiceImpl implements MovieService {
 
-	@Autowired
-	private MovieDao movieDao;
+    Predicate<MovieDTO> isDramaComedy = movie -> movie.getGenres().stream()
+            .anyMatch(g -> Arrays.asList(1, 2).contains(g.getId()));
+    BiPredicate<Movie, DirectorDTO> isDirector = (movie, director) -> movie.getDirectors().stream()
+            .anyMatch(d -> d.getId() == director.getId());
+    @Autowired
+    private MovieDao movieDao;
+    @Autowired
+    private ModelMapper mapper;
 
-	@Autowired
-	private ModelMapper mapper;
+    @Override
+    public List<DirectorDTO> directorsNumberMoviesAndGenres() {
+        return movieDao.findAllDirectors().stream()
+                .map(this::convertDirectorDTO)
+                .collect(Collectors.toList());
+    }
 
-	Predicate<MovieDTO> isDramaComedy = movie -> movie.getGenres().stream()
-			.filter(g -> Arrays.asList(1, 2).contains(g.getId())).findAny().isPresent();
+    @Override
+    public List<MovieDTO> moviesByDramaComedy() {
+        return movieDao.findAllMovies().stream()
+                .map(this::convertMovieDTO)
+                .filter(isDramaComedy)
+                .collect(Collectors.toList());
+    }
 
-	BiPredicate<Movie, DirectorDTO> isDirector = (movie, director) -> movie.getDirectors().stream()
-			.filter(d -> d.getId() == director.getId()).findAny().isPresent();
+    @Override
+    public Map<Integer, List<MovieDTO>> moviesByYears() {
+        return new TreeMap<>(movieDao.findAllMovies().stream()
+                .map(this::convertMovieDTO)
+                .collect(Collectors.groupingBy(MovieDTO::getYear)));
 
-	@Override
-	public List<DirectorDTO> directorsNumberMoviesAndGenres() {
-		return movieDao.findAllDirectors().stream().map(this::convertDirectorDTO).collect(Collectors.toList());
-	}
+    }
 
-	@Override
-	public List<MovieDTO> moviesByDramaComedy() {
-		return movieDao.findAllMovies().stream().map(this::convertMovieDTO).filter(isDramaComedy)
-				.collect(Collectors.toList());
-	}
+    private MovieDTO convertMovieDTO(Movie movie) {
+        return mapper.map(movie, MovieDTO.class);
+    }
 
-	@Override
-	public Map<Integer, List<MovieDTO>> moviesByYears() {
-		return new TreeMap<Integer, List<MovieDTO>>(movieDao.findAllMovies().stream().map(this::convertMovieDTO)
-				.collect(Collectors.groupingBy(MovieDTO::getYear)));
+    private DirectorDTO convertDirectorDTO(Director director) {
+        DirectorDTO directorDTO = mapper.map(director, DirectorDTO.class);
+        directorDTO.setNumberMovies(numberDirMovies(directorDTO));
+        directorDTO.setNumberGenres(numberDirGenres(directorDTO));
+        return directorDTO;
+    }
 
-	}
+    private long numberDirMovies(DirectorDTO director) {
+        return movieDao.findAllMovies().stream()
+                .filter(m -> isDirector.test(m, director)).count();
+    }
 
-	private MovieDTO convertMovieDTO(Movie movie) {
-		return mapper.map(movie, MovieDTO.class);
-	}
-
-	private DirectorDTO convertDirectorDTO(Director director) {
-		DirectorDTO directorDTO = mapper.map(director, DirectorDTO.class);
-		directorDTO.setNumberMovies(numberDirMovies(directorDTO));
-		directorDTO.setNumberGenres(numberDirGenres(directorDTO));
-		return directorDTO;
-	}
-
-	private long numberDirMovies(DirectorDTO director) {
-		return movieDao.findAllMovies().stream().filter(m -> isDirector.test(m, director)).count();
-	}
-
-	private long numberDirGenres(DirectorDTO director) {
-		return movieDao.findAllMovies().stream().filter(m -> isDirector.test(m, director)).map(m -> m.getGenres())
-				.flatMap(Collection::stream).collect(Collectors.toSet()).size();
-	}
+    private long numberDirGenres(DirectorDTO director) {
+        return movieDao.findAllMovies().stream()
+                .filter(m -> isDirector.test(m, director))
+                .map(Movie::getGenres)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet()).size();
+    }
 
 }
--- src/main/java/com/epam/streamPOC/service/impl/WorldServiceImpl.java
+++ src/main/java/com/epam/streamPOC/service/impl/WorldServiceImpl.java
@@ -1,74 +1,77 @@
 package com.epam.streamPOC.service.impl;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.epam.streamPOC.dao.InMemoryWorldDao;
 import com.epam.streamPOC.domain.City;
 import com.epam.streamPOC.domain.Country;
 import com.epam.streamPOC.service.WorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Service
 public class WorldServiceImpl implements WorldService {
 
-	@Autowired
-	private InMemoryWorldDao worldData;
+    @Autowired
+    private InMemoryWorldDao worldData;
 
-	private Predicate<City> isCapital = city -> worldData.findAllCountries().stream()
-			.filter(c -> c.getCapital() == city.getId()).findFirst().isPresent();
+    private Predicate<City> isCapital = city -> worldData.findAllCountries().stream()
+            .anyMatch(c -> c.getCapital() == city.getId());
 
-	private Function<City, String> getCountryContinent = city -> worldData.findCountryByCode(city.getCountryCode())
-			.getContinent();
+    private Function<City, String> getCountryContinent = city -> worldData.findCountryByCode(city.getCountryCode())
+            .getContinent();
 
-	@Override
-	public List<City> highestPopulatedByCountry() throws Exception {
-		return worldData.findAllCities().stream()
-				.collect(Collectors.groupingBy(City::getCountryCode,
-						Collectors.maxBy(Comparator.comparingInt(City::getPopulation))))
-				.values().stream().map(Optional::get).collect(Collectors.toList());
-	}
+    @Override
+    public List<City> highestPopulatedByCountry() {
+        return new ArrayList<>(worldData.findAllCities().stream()
+                .collect(Collectors.toMap(City::getCountryCode, Function.identity(),
+                        BinaryOperator.maxBy(Comparator.comparingInt(City::getPopulation))))
+                .values());
+    }
 
-	@Override
-	public List<City> highestPopulatedByContinent() throws Exception {
-		return worldData.findAllCities().stream()
-				.collect(Collectors.groupingBy(getCountryContinent,
-						Collectors.maxBy(Comparator.comparingInt(City::getPopulation))))
-				.values().stream().map(Optional::get).collect(Collectors.toList());
-	}
+    @Override
+    public List<City> highestPopulatedByContinent() {
+        return new ArrayList<>(worldData.findAllCities().stream()
+                .collect(Collectors.toMap(getCountryContinent, Function.identity(),
+                        BinaryOperator.maxBy(Comparator.comparingInt(City::getPopulation))))
+                .values());
+    }
 
-	@Override
-	public City highestPopulatedCapitalCity() throws Exception {
-		return worldData.findAllCities().stream().filter(c -> isCapital.test(c))
-				.max((c1, c2) -> Integer.compare(c1.getPopulation(), c2.getPopulation())).get();
-	}
+    @Override
+    public City highestPopulatedCapitalCity() {
+        return worldData.findAllCities().stream()
+                .filter(isCapital)
+                .max(Comparator.comparingInt(City::getPopulation))
+                .orElseThrow();
+    }
 
-	@Override
-	public List<City> highestPopulatedCapByContinent() throws Exception {
-		return worldData.findAllCities().stream().filter(c -> isCapital.test(c))
-				.collect(Collectors.groupingBy(getCountryContinent,
-						Collectors.maxBy(Comparator.comparingInt(City::getPopulation))))
-				.values().stream().map(Optional::get).collect(Collectors.toList());
-	}
+    @Override
+    public List<City> highestPopulatedCapByContinent() {
+        return new ArrayList<>(worldData.findAllCities().stream()
+                .filter(isCapital)
+                .collect(Collectors.toMap(getCountryContinent, Function.identity(),
+                        BinaryOperator.maxBy(Comparator.comparingInt(City::getPopulation))))
+                .values());
+    }
 
-	@Override
-	public List<Country> countriesByCityNumberDesc() throws Exception {
-		return worldData.findAllCountries().stream()
-				.sorted((c1, c2) -> Integer.compare(c2.getCities().size(), c1.getCities().size()))
-				.collect(Collectors.toList());
-	}
+    @Override
+    public List<Country> countriesByCityNumberDesc() {
+        return worldData.findAllCountries().stream()
+                .sorted(Comparator.comparing(country -> country.getCities().size()))
+                .collect(Collectors.toList());
+    }
 
-	@Override
-	public List<Country> countriesByPopulationDesc() throws Exception {
-		return worldData.findAllCountries().stream().filter(c -> c.getPopulation() > 0)
-				.sorted((c1, c2) -> Integer.compare(c2.getPopulation(), c1.getPopulation()))
-				.collect(Collectors.toList());
-	}
+    @Override
+    public List<Country> countriesByPopulationDesc() {
+        return worldData.findAllCountries().stream().filter(c -> c.getPopulation() > 0)
+                .sorted(Comparator.comparing(Country::getPopulation))
+                .collect(Collectors.toList());
+    }
 
 }
